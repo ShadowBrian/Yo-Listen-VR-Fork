e3808d5b2b711ea377c27074c8c611492dbf23bb
Brian Lindenhof <brian@shadowbrain.net>
2022-10-04 14:49:16 +0200

    Make canvas follower into a script that follows the head camera instead

--- Assets/Addons/Trabasura/CanvasFollower.cs
+++ Assets/Addons/Trabasura/CanvasFollower.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class CanvasFollower : MonoBehaviour
 {
+    public Transform playerhead;
+    public static CanvasFollower instance;
     // Start is called before the first frame update
     void Start()
     {
+        if(instance == null)
+        {
+            instance = this;
+        }
         if(RendererController.instance)   
         {
-            GetComponent<Canvas>().worldCamera=RendererController.instance.transform.GetChild(0).GetComponent<Camera>();
+            //GetComponent<Canvas>().worldCamera=RendererController.instance.transform.GetChild(0).GetComponent<Camera>();
         }
     }
 
+    public void Update()
+    {
+        if(playerhead != null)
+        {
+            transform.position = playerhead.position + playerhead.forward * 1.243f - playerhead.up * 0.742f;
+            transform.rotation = playerhead.rotation;
+            if (!playerhead.gameObject.activeInHierarchy)
+            {
+                Destroy(playerhead.root.gameObject);
+            }
+        }
+        else
+        {
+            playerhead = FindObjectOfType<MouseLook>().transform.GetChild(0);
+        }
+    }
 }
